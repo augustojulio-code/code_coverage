4906a648de0f3c4a676649b1216357a7fcc512b8
Julio <julio.mech@hotmail.com>
2024-10-06 16:49:10 -0300

    TDD implement

---
+++ src/test/java/com/backend/backend/repository/MessageRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.backend.backend.repository;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class MessageRepositoryTest {
+
+    @Test
+    void shootRegisterMessage() {
+        fail("Test not implemented");
+    }
+
+    @Test
+    void shootUpdateMessage() {
+        fail("Test not Implemented");
+    }
+
+    @Test
+    void shootDeleteMessage() {
+        fail("Test not Implemented");
+    }
+
+    @Test
+    void shootListMessage() {
+        fail("Test not Implemented");
+    }
+
+}
