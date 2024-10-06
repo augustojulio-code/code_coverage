a05ad825213345b944e5ce946b48405fd058bb0a
Julio <julio.mech@hotmail.com>
2024-10-06 16:29:02 -0300

    git flow init

--- src/main/java/com/backend/backend/domain/Message.java
+++ src/main/java/com/backend/backend/domain/Message.java
@@ -4,13 +4,24 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.Builder.Default;
 
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
 public class Message {
 
+    public Message() {
+    }
+
     @Id
-    private Long id;
+    private UUID id;
 
     @Column(nullable = false)
     private String user;
@@ -18,5 +29,8 @@ public class Message {
     private String content;
     @Builder.Default
     private LocalDateTime dateTimeNow;
+
+    @Default
+    private int like = 0;
 
 }
