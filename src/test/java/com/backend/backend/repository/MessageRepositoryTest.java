41bdb6ce7ee31cbabeeb81c3016cc071d94c7b75
Julio <julio.mech@hotmail.com>
2024-10-13 16:47:29 -0300

    unit test implement

--- src/test/java/com/backend/backend/repository/MessageRepositoryTest.java
+++ src/test/java/com/backend/backend/repository/MessageRepositoryTest.java
@@ -1,14 +1,50 @@
 package com.backend.backend.repository;
 
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class MessageRepositoryTest {
+import com.backend.backend.domain.Message;
+
+@SpringBootTest
+class MessageRepositoryTest {
+
+    @Mock
+    private MessageRepository repository;
+
+    private Message messageObj;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        messageObj = new Message();
+    }
 
     @Test
     void shootRegisterMessage() {
-        fail("Test not implemented");
+
+        messageObj.setId(UUID.randomUUID());
+        messageObj.setUsername("jose");
+        messageObj.setContent("Conteudo mensagem");
+
+        when(repository.save(messageObj)).thenReturn(messageObj);
+
+        Message messageSaved = repository.save(messageObj);
+
+        assertNotNull(messageSaved.getId());
+        verify(repository, times(1)).save(any(Message.class));
+
     }
 
     @Test
